d6fd959229b021e668ff2a8ff3d3208e5010cf88
SinowX <zhaoqifan0106@qq.com>
2021-06-27 00:17:22 +0800

    some update

--- test.js
+++ test.js
@@ -1,24 +1,2 @@
-const { R_OK } = require("constants")
-const readline=require("readline")
-const rl=readline.createInterface({
-    input:process.stdin,
-    output:process.stdout
-})
+const fs=require("fs")
 
-
-function doAync()
-{
-    return new Promise(
-        function(resolve,reject){
-            rl.question("Input\n",(str)=>{
-                console.log(str)
-                resolve(str)
-            })
-        }
-    )
-}
-
-doAync().then(()=>{
-    console.log(123)
-    return
-})
